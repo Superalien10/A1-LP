2f268743697c96a41298dad348cf287536373159
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-10-08 17:32:42 -0300

    Atualize pop-dur.py
    Crie as funções necessárias para se obter a popularidade e duração das músicas.

--- pop-dur.py
+++ pop-dur.py
@@ -1,1 +1,28 @@
+import requests
+import datetime
+from urllib.parse import urlencode
+import base64
 
+import busca_spotify as spo
+
+def encontrar_musica(musica, album, artista):
+    resposta = spo.spotify.search(query=musica, search_type="track")
+    resultado = []
+    for opcao in resposta["tracks"]["items"]:
+        print(opcao["album"]["name"], "\n", opcao["artists"][0]["name"])
+        if opcao["album"]["name"].lower()==album.lower() and opcao["artists"][0]["name"].lower()==artista.lower():
+            resultado.append(opcao)
+    assert len(resultado)==1, "Há mais de um registro de música nessas especificações."
+    return resultado
+
+def buscar_popularidade(fonte):
+    return fonte[0]["popularity"]
+
+def buscar_duracao(fonte):
+    return fonte[0]["duration_ms"]
+
+#Exemplo de uso
+"""
+fonte = encontrar_musica("Beds are burning", "Diesel and Dust", "Midnight Oil")
+print(popularidade(fonte), duracao(fonte))
+"""
