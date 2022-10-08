67b2896f90527894d44c260556be0c2a7897d641
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-10-08 18:36:40 -0300

    Update analise.py
    Responde questões quatro e dois.

--- respondendo/analise.py
+++ respondendo/analise.py
@@ -62,3 +62,49 @@ for album in albuns:
 """
 
 
+#Questão 4
+cola = []
+for album in albuns:
+    for musica in albuns[album]:
+        cola.append(musica)
+
+dados = {"Extra": cola}
+indices = []
+for album in albuns:
+    for musica in albuns[album]:
+        indices.append((album, musica))
+index = pd.MultiIndex.from_tuples(indices, names=["Álbum", "Música"])
+novo = pd.DataFrame(dados, index=index)
+
+ordem_vis = df.sort_values(by=["Duração"], ascending=False)
+ordem_vis = pd.concat([ordem_vis, novo], axis=1)
+ordem_vis = ordem_vis.loc[:,["Extra", "Duração"]]
+ordem_vis = ordem_vis.drop_duplicates(["Extra", "Duração"], keep='first')
+
+ordem_vis.drop("Extra", axis=1, inplace=True)
+mascara_desinformacao = ordem_vis["Duração"] != -1
+ordem_vis = ordem_vis.loc[mascara_desinformacao]
+
+mais_longas = ordem_vis.head(10)
+mais_curtas = ordem_vis.tail(10)
+print("Músicas mais longas: \n", mais_longas, "\nMúsicas mais curtas: \n", mais_curtas)
+mais_longas.to_csv(f"dataframe_mais_longas.csv", encoding="utf-8")
+mais_curtas.to_csv(f"dataframe_mais_curtas.csv", encoding="utf-8")
+
+#Questão 2
+
+cola = []
+for album in albuns:
+    for musica in albuns[album]:
+        cola.append(album)
+dados = {"Extra-Álbum": cola}
+novo = pd.DataFrame(dados, index=index)
+ordem_vis = pd.concat([ordem_vis, novo], axis=1)
+for album in albuns:
+    mascara_album = ordem_vis["Extra-Álbum"] == album
+    fim = copy.deepcopy(ordem_vis.loc[mascara_album])
+    fim.dropna(inplace=True)
+    fim.drop("Extra-Álbum", axis=1, inplace=True)
+    fim.to_csv(f"dataframe_duração_{album}.csv", encoding="utf-8")
+    print(fim)
+    
