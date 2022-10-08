b6c19dd008fc87dc28f14cb030a1d1e909ef4a62
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-10-08 17:53:10 -0300

    Atualize pop-dur.py

--- pop-dur.py
+++ pop-dur.py
@@ -3,8 +3,10 @@ import datetime
 from urllib.parse import urlencode
 import base64
 import numpy as np
+import pandas as pd
 
 import busca_spotify as spo
+import busca_discografia_2 as disco
 
 def encontrar_musica(musica, album, artista):
     resposta = spo.spotify.search(query=musica, search_type="track")
@@ -28,41 +30,38 @@ def encontrar_musica(musica, album, artista):
     except KeyError:
         resultado.append(-1)
         return resultado
+    
 
 def buscar_popularidade(fonte):
-    return fonte[0]["popularity"]
+    if fonte[0]==-1:
+        return -1
+    else:
+        return fonte[0]["popularity"]
 
 def buscar_duracao(fonte):
-    return fonte[0]["duration_ms"]
+    if fonte[0]==-1:
+        return -1
+    else:
+        return fonte[0]["duration_ms"]
 
-#Exemplo de uso
-"""
-fonte = encontrar_musica("Beds are burning", "Diesel and Dust", "Midnight Oil")
-print(popularidade(fonte), duracao(fonte))
-"""
 
-import pandas as pd
 
-import busca_discografia as disco
 
 artista = disco.recolher_artista()
 documento = disco.buscar_documento(artista)
 albuns = disco.buscar_albuns(documento)
 duracao = []
 popularidade = []
+visualizacoes = disco.buscar_views(documento)
 letra = disco.buscar_letra(documento)
+
 for album in albuns:
-    print("album")
     for musica in albuns[album]:
-        print("musica1")
-        try:
-            fonte = encontrar_musica(musica, album, artista)
-            duracao.append(buscar_duracao(fonte))
-            popularidade.append(buscar_popularidade(fonte))
-        except AssertionError as error:
-            print(error, "\n\n")
-        print("musica2")
-dados = {"Duração":duracao, "Popularidade":popoularidade, "Letra":letra}
+        fonte = encontrar_musica(musica, album, artista)
+        duracao.append(buscar_duracao(fonte))
+        popularidade.append(buscar_popularidade(fonte))
+dados = {"Duração":duracao, "Popularidade":popularidade, "Visualizações": visualizacoes, "Letra":letra}
+
 
 
 #Multi-índice
@@ -75,9 +74,14 @@ for album in albuns:
 index = pd.MultiIndex.from_tuples(indices, names=["Álbum", "Música"])
 print(dados)
 print(index)
-tabelinha = pd.DataFrame(dados, index=index)
+try:
+    tabelinha = pd.DataFrame(dados, index=index)
+except:
+    print(dados)
+    for array in dados:
+        print("Ahhh", dados[array], "\n", len(dados[array]))
+    tabelinha=pd.DataFrame(dados)
 print(tabelinha)
 nome = artista.replace(" ", "-")
 tabelinha.to_csv(f"dataframe_{nome}.csv", encoding="utf-8")
-#encontrar_musica("Tiptoe", "Night Vision", "Imagine Dragons")
 
