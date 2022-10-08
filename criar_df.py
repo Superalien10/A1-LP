d8dec2a2a76b5cdd035cb2134baa5a36a6a328de
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-10-08 20:29:27 -0300

    Update criar_df.py
    Insira a coluna prêmios

--- criar_df.py
+++ criar_df.py
@@ -4,6 +4,8 @@ from urllib.parse import urlencode
 import base64
 import numpy as np
 import pandas as pd
+from bs4 import BeautifulSoup as b
+import re
 
 import busca_spotify as spo
 import busca_discografia as disco
@@ -15,7 +17,6 @@ def encontrar_musica(musica, album, artista):
         for opcao in resposta["tracks"]["items"]:
             if opcao==None:
                 print("\n\n ok \n\n")
-            #elif opcao["name"].lower()==album.lower() and opcao["artists"][0]["name"].lower()==artista.lower():
             elif opcao["artists"][0]["name"].lower()==artista.lower():
                 resultado.append(opcao)
             """print("\n\n", opcao["name"], "+", album, "\n\n", opcao["artists"][0]["name"], "+", artista)
@@ -42,15 +43,18 @@ def buscar_duracao(fonte):
     if fonte[0]==-1:
         return -1
     else:
-        return fonte[0]["duration_ms"]
-
+        mili=fonte[0]["duration_ms"]
+        #mili = float("{:.2f}".format(mili))
+        return float(mili/1000)
 
 
 
 artista = disco.recolher_artista()
 documento = disco.buscar_documento(artista)
 albuns = disco.buscar_albuns(documento)
+
 duracao = []
+fonte_duração = []
 popularidade = []
 visualizacoes = disco.buscar_views(documento)
 letra = disco.buscar_letra(documento)
@@ -59,10 +63,8 @@ for album in albuns:
     for musica in albuns[album]:
         fonte = encontrar_musica(musica, album, artista)
         duracao.append(buscar_duracao(fonte))
+        fonte_duração.append(fonte)
         popularidade.append(buscar_popularidade(fonte))
-dados = {"Duração":duracao, "Popularidade":popularidade, "Visualizações": visualizacoes, "Letra":letra}
-
-
 
 #Multi-índice
 
@@ -72,6 +74,50 @@ for album in albuns:
         indices.append((album, musica))
 #print(indices)
 index = pd.MultiIndex.from_tuples(indices, names=["Álbum", "Música"])
+
+#Busca prêmios.
+
+pagina = requests.get("https://pt.wikipedia.org/wiki/Seu_Jorge").text
+documento = b(pagina, "html.parser")
+for album in albuns:
+    albuns[album].append(album)
+premiados = []
+for tabela in documento.find_all("table", attrs={"class":"wikitable"}):
+    linhas = tabela.find_all("tr")
+    for element in linhas:
+        element = element.find_all("td")
+        if len(element)>0:
+            if element[-1].text == "Venceu\n":
+                indicado = element[-2].text
+                indicado = indicado.replace(",", "")
+                indicado = indicado.replace("\n", "")
+                premiados.append(indicado)
+num=0
+premios = []
+for album in albuns:
+    for premio in premiados:
+        for item in albuns[album]:
+            item.replace(",", "")
+            item.replace(" ", "*")
+            item = "^"+item
+            if re.search(item,premio):
+                num+=1
+    premios.append(num)
+    num=0
+posicao=0
+premios_final = []
+for album in albuns:
+    qual = len(albuns[album])-1
+    while qual > 0:
+        qual-=1
+        premios_final.append(premios[posicao])
+    posicao+=1
+
+#Criação dos dados.
+dados = {"Duração":duracao, "Popularidade":popularidade, "Visualizações": visualizacoes, "Letra":letra, "Prêmios": premios_final}
+
+
+
 print(dados)
 print(index)
 try:
@@ -81,7 +127,11 @@ except:
     for array in dados:
         print("Ahhh", dados[array], "\n", len(dados[array]))
     tabelinha=pd.DataFrame(dados)
+tabelinha = tabelinha.astype({'Visualizações':'int'})
 print(tabelinha)
 nome = artista.replace(" ", "-")
 tabelinha.to_csv(f"dataframe_{nome}.csv", encoding="utf-8")
+
+
+
 
