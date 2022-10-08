3f66c58f8f7e84b5dc213f0178e5afb4b2213e7e
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-10-08 17:43:55 -0300

    Atualize pop-dur.py
    Atualize a função encontrar_musica() e os módulos importados.

--- pop-dur.py
+++ pop-dur.py
@@ -2,18 +2,32 @@ import requests
 import datetime
 from urllib.parse import urlencode
 import base64
+import numpy as np
 
 import busca_spotify as spo
 
 def encontrar_musica(musica, album, artista):
     resposta = spo.spotify.search(query=musica, search_type="track")
     resultado = []
-    for opcao in resposta["tracks"]["items"]:
-        print(opcao["album"]["name"], "\n", opcao["artists"][0]["name"])
-        if opcao["album"]["name"].lower()==album.lower() and opcao["artists"][0]["name"].lower()==artista.lower():
-            resultado.append(opcao)
-    assert len(resultado)==1, "Há mais de um registro de música nessas especificações."
-    return resultado
+    try:
+        for opcao in resposta["tracks"]["items"]:
+            if opcao==None:
+                print("\n\n ok \n\n")
+            #elif opcao["name"].lower()==album.lower() and opcao["artists"][0]["name"].lower()==artista.lower():
+            elif opcao["artists"][0]["name"].lower()==artista.lower():
+                resultado.append(opcao)
+            """print("\n\n", opcao["name"], "+", album, "\n\n", opcao["artists"][0]["name"], "+", artista)
+            print("AQUI", opcao)"""
+            try:
+                assert len(resultado)==1, "Há mais de um registro de música nessas especificações."
+            except AssertionError as error:
+                 print(error, "\n\n")
+            if len(resultado)==0:
+                resultado.append(-1)
+            return resultado
+    except KeyError:
+        resultado.append(-1)
+        return resultado
 
 def buscar_popularidade(fonte):
     return fonte[0]["popularity"]
