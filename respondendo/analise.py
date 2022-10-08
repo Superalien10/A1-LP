79c0e641b78dfd785b1b5249dd1f3f4e462b9662
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-10-08 18:31:56 -0300

    Update analise.py
    Gera .csv's que respondem à questão dois.

--- respondendo/analise.py
+++ respondendo/analise.py
@@ -9,14 +9,13 @@ sys.path.insert(0, "..")
 import busca_discografia_2 as disco
 
 df = pd.read_csv('../dataframe_seu-jorge.csv', index_col=["Álbum","Música"])
-print(df.head())
+#print(df.head())
 
 documento = disco.buscar_documento("seu jorge")
 albuns = disco.buscar_albuns(documento)
 cola = []
 for album in albuns:
     for musica in albuns[album]:
-        print(musica)
         cola.append(musica)
 #cola=pd.Series(cola)
 
@@ -30,29 +29,26 @@ novo = pd.DataFrame(dados, index=index)
 
 
 
-print(df.sort_values(by=["Álbum"]))
-ordem_vis = df.sort_values(by=["Visualizações"])
-#novo = ordem_vis.iloc[:,"Música"]
+
+ordem_vis = df.sort_values(by=["Visualizações"], ascending=False)
 ordem_vis = pd.concat([ordem_vis, novo], axis=1)
-print(ordem_vis)
 ordem_vis.to_csv(f"dataframe_nome.csv", encoding="utf-8")
-#print(ordem_vis.drop_duplicates(["Música"], keep='first'))
 ordem_vis = ordem_vis.loc[:,["Extra", "Visualizações"]]
-print(ordem_vis)
-print(ordem_vis.drop_duplicates(["Extra", "Visualizações"], keep='first'))
+ordem_vis = ordem_vis.drop_duplicates(["Extra", "Visualizações"], keep='first')
 
 
-"""
-for part in documento.find_all("a", attrs={"class":"bt-play-song"}):
-        musica_link = part.attrs.get("href")
-        musica_doc=b(r.get(f"https://www.letras.mus.br{musica_link}").text, "html.parser")
-        try:
-            view = musica_doc.find("div", attrs={"class":"cnt-info_exib"}).b.text
-            #pd.to_numeric(view)
-            #view = float(view)
-            view = view.replace(".", "")
-            view = int(view)
-            print(view, type(view))
-        except AttributeError:
-            print("ops")
-"""
+
+ordem_vis.drop("Extra", axis=1, inplace=True)
+mascara_desinformacao = ordem_vis["Visualizações"] != -1
+ordem_vis = ordem_vis.loc[mascara_desinformacao]
+
+
+
+
+mais_ouvidas = ordem_vis.head(10)
+menos_ouvidas = ordem_vis.tail(10)
+print(type(mais_ouvidas))
+print("Músicas mais ouvidas: \n", mais_ouvidas, "\nMúsicas menos ouvidas: \n", menos_ouvidas)
+mais_ouvidas.to_csv(f"dataframe_mais_ouvidas.csv", encoding="utf-8")
+menos_ouvidas.to_csv(f"dataframe_menos_ouvidas.csv", encoding="utf-8")
+
