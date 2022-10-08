5872e8898d16b3f0daceffff42a3c01b70db57f8
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-10-08 17:35:44 -0300

    Atualize pop-dur.py
    Tente criar o esboço dos dados obtidos com busca_spotify.py, busca_discografia.py e este módulo.

--- pop-dur.py
+++ pop-dur.py
@@ -26,3 +26,28 @@ def buscar_duracao(fonte):
 fonte = encontrar_musica("Beds are burning", "Diesel and Dust", "Midnight Oil")
 print(popularidade(fonte), duracao(fonte))
 """
+
+import pandas as pd
+
+import busca_discografia as disco
+
+artista = disco.recolher_artista()
+documento = disco.buscar_documento(artista)
+albuns = disco.buscar_albuns(documento)
+duracao = []
+popularidade = []
+letra = disco.buscar_letra(documento)
+for album in albuns:
+    print("album")
+    for musica in albuns[album]:
+        print("musica1")
+        try:
+            fonte = encontrar_musica(musica, album, artista)
+            duracao.append(buscar_duracao(fonte))
+            popularidade.append(buscar_popularidade(fonte))
+        except AssertionError as error:
+            print(error, "\n\n")
+        print("musica2")
+dados = {"Duração":duracao, "Popularidade":popoularidade, "Letra":letra}
+
+print(dados)
